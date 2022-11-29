3598b7b50b6729398917f74f22e01c619c2edbea
Mateo-olier <aolierguerrero@outlook.com>
2022-11-28 19:11:10 -0500

    Backend listo

--- src/main/java/com/example/mascota/controller/PostController.java
+++ src/main/java/com/example/mascota/controller/PostController.java
@@ -1,6 +1,6 @@
 package com.example.mascota.controller;
+
 import com.example.mascota.model.Post;
-import com.example.mascota.model.User;
 import com.example.mascota.repository.PostRepository;
 import com.example.mascota.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/api/post")
@@ -22,23 +21,66 @@ public class PostController {
     @Autowired
     UserRepository userRepository;
 
+    //Add post
     @PostMapping("/add/{id}")
-    public ResponseEntity<Post> createPost(@PathVariable(value = "id") String id, @RequestBody Post requestPost) {
-      try {
-          Post _post = userRepository.findById(id).map(user -> {
-              requestPost.setUserID(id);
-              return postRepository.save(requestPost);
-          }).orElseThrow();
-          return new ResponseEntity<>(_post, HttpStatus.CREATED);
-      }catch (Exception e){
-          return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+    public ResponseEntity<Post> createPost(@PathVariable("id") String id, @RequestBody Post requestPost) {
+        try {
+            Post _post = userRepository.findById(id).map(user -> {
+                requestPost.setUserID(id);
+                return postRepository.save(requestPost);
+            }).orElseThrow();
+            return new ResponseEntity<>(_post, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    //update post
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable("id") String id, @RequestBody Post requestPost) {
+        Optional<Post> searchPost = postRepository.findById(id);
+        if (searchPost.isPresent()) {
+            Post postUp = searchPost.get();
+            if (requestPost.getDescription() == null) {
+                postUp.setDescription(postUp.getDescription());
+            } else {
+                postUp.setDescription(requestPost.getDescription());
+            }
+            if (requestPost.getReward() == 0) {
+                postUp.setReward(postUp.getReward());
+            } else {
+                postUp.setReward(requestPost.getReward());
+            }
+            return new ResponseEntity<>(postRepository.save(postUp), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //List posts
     @GetMapping("/posts")
     public ResponseEntity<List<Post>> getAllPost() {
         List<Post> posts = postRepository.findAll();
         return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
+    //Post one
+    @GetMapping("/one/{id}")
+    public ResponseEntity<Post> getPost(@PathVariable("id") String id) {
+        Optional<Post> searchPost = postRepository.findById(id);
+        if (searchPost.get() != null) {
+            return new ResponseEntity<>(searchPost.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deletePost(@PathVariable("id")String id){
+        try{
+            postRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/com/example/mascota/controller/UserController.java
+++ src/main/java/com/example/mascota/controller/UserController.java
@@ -93,7 +93,7 @@ public class UserController {
     public ResponseEntity<HttpStatus> deleteUSer(@PathVariable("id") String id) {
         try {
             userRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
